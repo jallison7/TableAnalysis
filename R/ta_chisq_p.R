127a0adc293b1f2b8c1401589d4eb233fe742bd5
James Allison <jallison@byu.edu>
2021-03-29 11:26:59 -0600

    Update ta_chisq_p.R

--- R/ta_chisq_p.R
+++ R/ta_chisq_p.R
@@ -3,7 +3,7 @@ ta_chisq_p <- function(Table) {
   cols <- ncol(Table)
   x <- ta_chi_square(Table)
   df <- (rows - 1) * (cols - 1)
-  p <- (1 - pchisq(x,df))
+  p <- pchisq(x,df, lower.tail = FALSE)
   options(digits = 3, scipen = 999)
   return(p)
 }
