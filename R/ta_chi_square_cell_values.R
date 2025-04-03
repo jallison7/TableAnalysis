772b2a34d4e507ef8e6db802a4c572ed1e8588bc
James Allison <jallison@byu.edu>
2025-04-03 17:39:03 -0600

    Update ta_chi_square_cell_values.R
    fixing a bug in the function

--- R/ta_chi_square_cell_values.R
+++ R/ta_chi_square_cell_values.R
@@ -1,6 +1,6 @@
 ta_chi_square_cell_values <- function(Table) {
   expected <- ta_expected(Table)
-  cell_values <- sqrt((Table - expected)^2 / expected)
+  cell_values <- (Table - expected)^2 / expected
   return(cell_values)
 }
 
